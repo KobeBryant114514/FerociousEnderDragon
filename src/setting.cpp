d79bf962d9ffc7a3f8bc6944e90976265f64216b
Tsubasa6848 <116721335+Tsubasa6848@users.noreply.github.com>
2023-03-06 07:56:32 +0800

    change death messages

--- src/setting.cpp
+++ src/setting.cpp
@@ -224,7 +224,7 @@ void loadConfig() {
         json["death.ferociousEnderDragon.explosion"] = "%1$s只因再多看了一眼末影龙就爆炸了";
         json["death.ferociousEnderDragon.lightning"] = "%1$s败落于末影龙制造的闪电之下";
         json["death.ferociousEnderDragon.dragonBreath"] = "%1$s被末影龙的龙息烤熟了";
-        json["death.ferociousEnderDragon.collision"] = "%1$s没能躲过末影龙的冲撞";
+        json["death.ferociousEnderDragon.collision"] = "%1$s最终还是没能躲过末影龙的冲撞";
         json["death.ferociousEnderDragon.sonicBoom"] = "%1$s只因再靠近了一点末影龙就融化了";
         DefaultFile << json.dump(4);
         DefaultFile.close();
